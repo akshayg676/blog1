b92f89c7dd5c2d4d9210822a9e6b9b3e48877a5b
Akshay G <akshayg676@gmail.com>
2021-07-27 18:02:54 +0530

    complete update post

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/header/Header.jsx";
 import Home from "./components/home/Home.jsx";
 import DetailedPost from "./components/post/DetailedPost.jsx";
 import CreatePost from "./components/post/CreatePost.jsx";
+import UpdatePost from "./components/post/UpdatePost.jsx";
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/details" component={DetailedPost} />
           <Route path="/create" component={CreatePost} />
+          <Route path="/update" component={UpdatePost} />
         </Switch>
       </Box>
     </BrowserRouter>
