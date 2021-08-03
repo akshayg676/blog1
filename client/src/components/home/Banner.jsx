7cb44d0bd4f3393e54fabc91e4162fe2db612745
Akshay G <akshayg676@gmail.com>
2021-08-03 11:46:15 +0530

    banner text updated

--- client/src/components/home/Banner.jsx
+++ client/src/components/home/Banner.jsx
@@ -1,6 +1,6 @@
 import { makeStyles, Box, Typography } from "@material-ui/core";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   image: {
     height: "50vh",
     width: "100%",
@@ -9,21 +9,30 @@ const useStyles = makeStyles({
     flexDirection: "column",
     justifyContent: "center",
     "& :first-child": {
-      fontSize: 50,
+      fontSize: 100,
       textAlign: "center",
+      fontFamily: "Roboto",
+      [theme.breakpoints.down("sm")]: {
+        fontSize: 80,
+      },
+      [theme.breakpoints.down("xs")]: {
+        fontSize: 50,
+      },
     },
     "& :last-child": {
       fontSize: 30,
       textAlign: "center",
     },
   },
-});
+}));
 
 function Banner() {
   const classes = useStyles();
   return (
     <Box className={classes.image}>
-      <Typography>“Create content that teaches.”</Typography>
+      <Typography className={classes.quote}>
+        “Create content that teaches.”
+      </Typography>
       <Typography>-Neil Patel</Typography>
     </Box>
   );
